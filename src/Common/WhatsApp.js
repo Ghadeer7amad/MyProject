2022d1d1756841a6736a0c2dc14574f939352075
Maymonakh <maymonakhanfar@gmail.com>
2024-01-15 17:29:04 +0200

    add whatsapp

--- src/Common/WhatsApp.js
+++ src/Common/WhatsApp.js
@@ -1,22 +1,36 @@
-import React from 'react';
-import { View, TouchableOpacity, Linking } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import ActionButton from 'react-native-action-button';
+import React from "react";
+import { View, TouchableOpacity, Linking } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import ActionButton from "react-native-action-button";
 import Color from "../Common/Color.js";
 
-
 const WhatsApp = () => {
-    const openWhatsAppChat = () => {
-      const whatsappNumber = '+970592953343'; 
-      const url = `https://api.whatsapp.com/send?phone=${whatsappNumber}`;
-      Linking.openURL(url);
-    };
-  
-    return (
-      <TouchableOpacity onPress={openWhatsAppChat} style={{ position: 'absolute', bottom: 70, right: 22,zIndex:1000,backgroundColor:Color.primary,padding:20,borderRadius:50 }}>
-        <Icon name="whatsapp" size={50} color='white' />
-      </TouchableOpacity>
-    );
+  const openWhatsAppChat = () => {
+    const whatsappNumber = "+970592953343";
+    const url = `https://api.whatsapp.com/send?phone=${whatsappNumber}`;
+    Linking.openURL(url);
   };
 
-  export default WhatsApp;
+  return (
+    <TouchableOpacity
+      onPress={openWhatsAppChat}
+      style={{
+        position: "absolute",
+        bottom: 70,
+        right: 22,
+        zIndex: 1000,
+        backgroundColor: Color.primary,
+        padding: 20,
+        borderRadius: 50, 
+        width: 80, 
+        height: 80,
+        justifyContent: "center", 
+        alignItems: "center", 
+      }}
+    >
+      <Icon name="whatsapp" size={40} color="white" />
+    </TouchableOpacity>
+  );
+};
+
+export default WhatsApp;
